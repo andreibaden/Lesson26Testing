95821ab2e2ff2508bf58a2b659262c941e158d46
andreibaden@gmail.com <itstep_student@outlook.com>
2023-04-27 21:49:31 +0300

    Past 3

--- bus.py
+++ bus.py
@@ -9,6 +9,23 @@ class Bus:
         self._price = price    # protected
         self._number = number  # protected
 
+    def get_brand(self):
+        return self.__brand
+
+    def set_brand(self, name):
+        self.__brand = name
+
+    def get_number(self):
+            return self.__number
+
+    def set_number(self, name):
+            self.__number = name
+    def get_price(self):
+        return self.__price
+
+    def set_price(self, name):
+            self.__number = name
+
     # def __del__(self):
     #     print(f"destructor for {self.brand}")
 
@@ -17,13 +34,16 @@ class Bus:
     f"number = {self._number}"
 
 if __name__ == "__main__":
-    bus1 = Bus("Scania", 23000, 42)
-    bus2 = Bus("MAZ", 12000, 34)
-    bus3 = Bus("Volvo", 31000, 48)
-    bus4 = Bus("PAZ", 11000, 15)
-    bus5 = Bus("Mercedes Benz", 123000, 67)
-    # print(bus1.__str__())
-    print(Bus._number)
+    bus = Bus()
+
+
+    # bus1 = Bus("Scania", 23000, 42)
+    # bus2 = Bus("MAZ", 12000, 34)
+    # bus3 = Bus("Volvo", 31000, 48)
+    # bus4 = Bus("PAZ", 11000, 15)
+    # bus5 = Bus("Mercedes Benz", 123000, 67)
+    # # print(bus1.__str__())
+    # print(Bus._number)
 
 
 
